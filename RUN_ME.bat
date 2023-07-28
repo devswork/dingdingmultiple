9e781208bf9b503fe7b600b99d7e003b655b1966
tian <devswork@163.com>
2023-07-28 16:51:48 +0800

    fix bug

--- RUN_ME.bat
+++ RUN_ME.bat
@@ -1,4 +1,6 @@
 @echo off
+cd /d %~dp0
+
 echo "******************************************************"
 echo "*                  DingDingMultiple                  *"
 echo "*                               TIAN                 *"
